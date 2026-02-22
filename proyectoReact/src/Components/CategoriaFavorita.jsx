c65a17499171094ca63867708de87861321411c8
Juan <juanpipapa2002@gmail.com>
2026-02-22 17:22:12 -0300

    Categoria favorita

--- proyectoReact/src/Components/CategoriaFavorita.jsx
+++ proyectoReact/src/Components/CategoriaFavorita.jsx
@@ -1,21 +1,53 @@
-import React, { use } from 'react'
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 
 const CategoriaFavorita = () => {
 
-    const dispach = useDispatch();
-    const categoria = useSelector(state => state.categoria.categoriaFavorita);
+  const peliculas = useSelector(
+    state => state.peliculas.todasPeliculas
+  );
 
-     
+  const categorias = useSelector(
+    state => state.peliculas.categorias
+  );
 
+  
+  const categoriaFavorita = useMemo(() => {
 
+    if (!peliculas || peliculas.length === 0) return null;
 
+  
+    const conteo = peliculas.reduce((acc, peli) => {
+      acc[peli.idCategoria] = (acc[peli.idCategoria] || 0) + 1;
+      return acc;
+    }, {});
 
-    return (
-    <div>CategoriaFavorita</div>
+    
+    const max = Math.max(...Object.values(conteo));
 
+    
+    const idFavorito = Number(
+      Object.keys(conteo).find(id => conteo[id] === max)
+    );
 
+    
+    return categorias.find(c => c.id === idFavorito);
 
-  )
-}
+  }, [peliculas, categorias]);
 
-export default CategoriaFavorita
+  if (!categoriaFavorita) {
+    return <p>No hay categoría favorita</p>;
+  }
+
+  return (
+    <div>
+      <h2>Categoría Favorita</h2>
+       
+        <p>{categoriaFavorita.emoji}</p>
+        
+      <p>{categoriaFavorita.nombre}</p>
+    </div>
+  );
+};
+
+export default CategoriaFavorita;
